f7207a02861ff9f2c1a86790c69b98d2d69cde69
799516423@qq.com <github_jachs22>
2020-09-06 22:00:52 +0800

    哈拉少

--- src/com/jachs/hunDouLuo/ui/Imgs.java
+++ src/com/jachs/hunDouLuo/ui/Imgs.java
@@ -6,15 +6,17 @@
  /****
   * 图片初始化实体
   * @author zhanchaohan
-  *
+  * funny文件夹下图片素材纯属恶搞
   */
  public class Imgs
  {
    public static Image STRONGBULLET = new ImageIcon("graphics/bullet/strongBullet.png").getImage();
    public static Image NUNLINERBULLET = new ImageIcon("graphics/bullet/nunLinerBullet.png").getImage();
-   public static Image STAND02 = new ImageIcon("graphics/hero/stand02.png").getImage();
+//   public static Image STAND02 = new ImageIcon("graphics/hero/stand02.png").getImage();
+   public static Image STAND02 = new ImageIcon("graphics/hero/funny/stand02.png").getImage();
    public static Image STAND06 = new ImageIcon("graphics/hero/stand06.png").getImage();
-   public static Image STAND2 = new ImageIcon("graphics/hero/stand2.png").getImage();
+//   public static Image STAND2 = new ImageIcon("graphics/hero/stand2.png").getImage();
+   public static Image STAND2 = new ImageIcon("graphics/hero/funny/stand2.png").getImage();
    public static Image STAND6 = new ImageIcon("graphics/hero/stand6.png").getImage();
    public static Image WALKANDSHOT2 = new ImageIcon("graphics/hero/walkandshot2.png").getImage();
    public static Image WALKANDSHOT3 = new ImageIcon("graphics/hero/walkandshot3.png").getImage();
@@ -30,7 +32,8 @@
    public static Image ENEMYBOMBING = new ImageIcon("graphics/enemy/enemyBombing.png").getImage();
    public static Image JUMP2 = new ImageIcon("graphics/hero/jump2.png").getImage();
    public static Image JUMP6 = new ImageIcon("graphics/hero/jump6.png").getImage();
-   public static Image CRAWL2 = new ImageIcon("graphics/hero/crawl2.png").getImage();
+//   public static Image CRAWL2 = new ImageIcon("graphics/hero/crawl2.png").getImage();
+   public static Image CRAWL2 = new ImageIcon("graphics/hero/funny/crawl2.png").getImage();
    public static Image CRAWL6 = new ImageIcon("graphics/hero/crawl6.png").getImage();
    public static Image BACKGROUND01 = new ImageIcon("graphics/backgrounds/bg_01.png").getImage();
    public static Image GRASS = new ImageIcon("graphics/backgrounds/Grass.png").getImage();
