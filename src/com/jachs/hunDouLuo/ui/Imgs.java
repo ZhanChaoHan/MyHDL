fdba2d00e1b0690729fa930501522c1f555b170d
799516423@qq.com <github_jachs22>
2020-09-06 19:21:01 +0800

    first commit

---
+++ src/com/jachs/hunDouLuo/ui/Imgs.java
@@ -0,0 +1,40 @@
+ package com.jachs.hunDouLuo.ui;
+ 
+ import java.awt.Image;
+ import javax.swing.ImageIcon;
+ 
+ public class Imgs
+ {
+   public static Image STRONGBULLET = new ImageIcon("graphics/bullet/strongBullet.png").getImage();
+   public static Image NUNLINERBULLET = new ImageIcon("graphics/bullet/nunLinerBullet.png").getImage();
+   public static Image STAND02 = new ImageIcon("graphics/hero/stand02.png").getImage();
+   public static Image STAND06 = new ImageIcon("graphics/hero/stand06.png").getImage();
+   public static Image STAND2 = new ImageIcon("graphics/hero/stand2.png").getImage();
+   public static Image STAND6 = new ImageIcon("graphics/hero/stand6.png").getImage();
+   public static Image WALKANDSHOT2 = new ImageIcon("graphics/hero/walkandshot2.png").getImage();
+   public static Image WALKANDSHOT3 = new ImageIcon("graphics/hero/walkandshot3.png").getImage();
+   public static Image WALKANDSHOT5 = new ImageIcon("graphics/hero/walkandshot5.png").getImage();
+   public static Image WALKANDSHOT6 = new ImageIcon("graphics/hero/walkandshot6.png").getImage();
+   public static Image WALKANDSHOT1 = new ImageIcon("graphics/hero/walkandshot1.png").getImage();
+   public static Image WALKANDSHOT7 = new ImageIcon("graphics/hero/walkandshot7.png").getImage();
+   public static Image HERODIEING = new ImageIcon("graphics/hero/dieing.png").getImage();
+   public static Image ENEMYWALK2 = new ImageIcon("graphics/enemy/walk2.png").getImage();
+   public static Image ENEMYWALK6 = new ImageIcon("graphics/enemy/walk6.png").getImage();
+   public static Image SIMPLEENEMYDIEING2 = new ImageIcon("graphics/enemy/simpleEnemyDieing2.png").getImage();
+   public static Image SIMPLEENEMYDIEING6 = new ImageIcon("graphics/enemy/simpleEnemyDieing6.png").getImage();
+   public static Image ENEMYBOMBING = new ImageIcon("graphics/enemy/enemyBombing.png").getImage();
+   public static Image JUMP2 = new ImageIcon("graphics/hero/jump2.png").getImage();
+   public static Image JUMP6 = new ImageIcon("graphics/hero/jump6.png").getImage();
+   public static Image CRAWL2 = new ImageIcon("graphics/hero/crawl2.png").getImage();
+   public static Image CRAWL6 = new ImageIcon("graphics/hero/crawl6.png").getImage();
+   public static Image BACKGROUND01 = new ImageIcon("graphics/backgrounds/bg_01.png").getImage();
+   public static Image GRASS = new ImageIcon("graphics/backgrounds/Grass.png").getImage();
+   public static Image ENEMYSHOT1 = new ImageIcon("graphics/enemy/shot1.png").getImage();
+   public static Image ENEMYSHOT3 = new ImageIcon("graphics/enemy/shot3.png").getImage();
+   public static Image ENEMYJUMP2 = new ImageIcon("graphics/enemy/enemyJump2.png").getImage();
+   public static Image ENEMYJUMP6 = new ImageIcon("graphics/enemy/enemyJump6.png").getImage();
+   public static Image BOSS = new ImageIcon("graphics/enemy/boss.png").getImage();
+   public static Image BOSSBOMBBED = new ImageIcon("graphics/enemy/bossBombbed.png").getImage();
+   public static Image BOSSCHILD = new ImageIcon("graphics/enemy/bossChild.png").getImage();
+   public static Image BOSSBOMBBING = new ImageIcon("graphics/enemy/bossBomb.png").getImage();
+ }
